e63c37fd862ce313728eed68310c39f6591058a9
Igor Khrupin <igor@hrupin.com>
2015-02-23 23:54:49 +0200

    changed package

---
+++ demo/src/com/hrupin/clusters/demo/Coffee.java
@@ -0,0 +1,21 @@
+package com.hrupin.clusters.demo;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class Coffee implements ClusterItem {
+    public final String name;
+    public final int profilePhoto;
+    private final LatLng mPosition;
+
+    public Coffee(LatLng position, String name, int pictureResource) {
+        this.name = name;
+        profilePhoto = pictureResource;
+        mPosition = position;
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+}
